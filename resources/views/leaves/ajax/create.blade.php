3eb22e07b8ed0ea9156b3493b7bb857dd289b978
uzair baig <uzairbaig040@gmail.com>
2026-05-14 09:56:58 +0500

    Revert "leave dropdown"
    This reverts commit 9f412d0155a184b2100d2e27f5d92b9b6e66d47f.

--- resources/views/leaves/ajax/create.blade.php
+++ resources/views/leaves/ajax/create.blade.php
@@ -8,8 +8,6 @@
 <link rel="stylesheet" href="{{ asset('vendor/css/dropzone.min.css') }}">
 <link rel="stylesheet" href="{{ asset('vendor/css/daterangepicker.css') }}">
 
-<meta name="turbo-cache-control" content="no-cache">
-
 <div class="row">
     <div class="col-sm-12">
         <x-form id="save-lead-data-form">
@@ -272,16 +270,17 @@
                 });
 
                 if ($('#user_id').val() != '') {
-                    const employeeID = $('#user_id').val();
-                    setMinDate(employeeID);
-                    getEmployeeLeaveTypes(employeeID);
+                    setMinDate($('#user_id').val());
                 }
 
-                function getEmployeeLeaveTypes(employeeID) {
+                $leaveBody.on('change' + namespace, '#user_id', function(e) {
+                    const employeeID = $(this).val();
+                    setMinDate(employeeID);
+                    
                     let id = employeeID || 0;
                     var url = "{{ route('employee-leaves.employee_leave_types', ':id') }}";
                     url = url.replace(':id', id);
-
+                    
                     $.easyAjax({
                         url: url,
                         type: "GET",
@@ -294,12 +293,6 @@
                             }
                         }
                     });
-                }
-
-                $leaveBody.on('change' + namespace, '#user_id', function(e) {
-                    const employeeID = $(this).val();
-                    setMinDate(employeeID);
-                    getEmployeeLeaveTypes(employeeID);
                 });
 
                 function setMinDate(employeeID) {
